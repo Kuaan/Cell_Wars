57766e872883d7698f8a688aa191009aa5d29c6a
Angus <89236757+Kuaan@users.noreply.github.com>
2026-02-04 23:17:58 +0800

    Update client_config.py

--- client_config.py
+++ client_config.py
@@ -1,1 +1,10 @@
+# client_config.py
+GITHUB_USER = "Kuaan"
+GITHUB_REPO = "Cell_Wars"
+ASSETS_BASE = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/main/assets/"
+SOUNDS_BASE = f"{ASSETS_BASE}sounds/"
+SERVER_URL = "https://cell-wars.onrender.com"
 
+# 這裡可以定義初始音量或其他前端參數
+DEFAULT_VOL_BGM = 0.4
+DEFAULT_VOL_SFX = 0.6
